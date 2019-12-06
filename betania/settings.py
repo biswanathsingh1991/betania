645f134196384be7a4d2725f971b674c37c36368
Ubuntu <ubuntu@ip-172-31-44-85.ap-south-1.compute.internal>
2019-12-06 11:28:09 +0000

    df

--- betania/settings.py
+++ betania/settings.py
@@ -60,7 +60,9 @@ ROOT_URLCONF = 'betania.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+		'/home/ubuntu/betania/templates',
+],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
