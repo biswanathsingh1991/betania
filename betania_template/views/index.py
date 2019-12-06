bb3520fe26dd780183b7118de49450875a20a039
Ubuntu <ubuntu@ip-172-31-44-85.ap-south-1.compute.internal>
2019-12-06 11:37:10 +0000

    skdjf

--- betania_template/views/index.py
+++ betania_template/views/index.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 class IndexTemplateView(TemplateView):
     http_method_names = ['get', 'post', 'put',
                          'patch', 'delete', 'head', 'options', 'trace']
-    template_name = 'l.html'
+    template_name = 'tw/index-1.html'
 
     def get(self, request, *args, **kwargs):
 
