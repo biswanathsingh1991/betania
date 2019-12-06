394bb90b07e0f93d8183bbd7773da537f345bf9b
biswanath <biswanathsingh1991@gmail.com>
2019-12-06 17:08:10 +0530

    kjhsf

---
+++ templates/app-services/flash.service.js
@@ -0,0 +1,58 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('FlashService', FlashService);
+
+    FlashService.$inject = ['$rootScope', 'toastr'];
+    function FlashService($rootScope, toastr) {
+        var service = {};
+
+        service.Success = Success;
+        service.Error = Error;
+        service.Toaster = ShowToaster;
+        initService();
+
+        return service;
+
+        function initService() {
+            $rootScope.$on('$locationChangeStart', function () {
+                clearFlashMessage();
+            });
+
+            function clearFlashMessage() {
+                var flash = $rootScope.flash;
+                if (flash) {
+                    if (!flash.keepAfterLocationChange) {
+                        delete $rootScope.flash;
+                    } else {
+                        // only keep for a single location change
+                        flash.keepAfterLocationChange = false;
+                    }
+                }
+            }
+        }
+
+        function Success(message, keepAfterLocationChange) {
+            $rootScope.flash = {
+                message: message,
+                type: 'success',
+                keepAfterLocationChange: keepAfterLocationChange
+            };
+        }
+
+        function Error(message, keepAfterLocationChange) {
+            $rootScope.flash = {
+                message: message,
+                type: 'error',
+                keepAfterLocationChange: keepAfterLocationChange
+            };
+        }
+
+        function ShowToaster(message, type, title) {
+            toastr.error(message,title);
+        }
+    }
+
+})();
---
+++ templates/pages/login/login.controller.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('LoginController', LoginController);
+
+    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
+    function LoginController($location, AuthenticationService, FlashService) {
+        var vm = this;
+
+        localStorage.users =null;
+
+        localStorage.users = `[{"firstName":"admin","lastName":"admin","username":"admin","password":"password","id":1}]`;
+
+
+        vm.login = login;
+
+        (function initController() {
+            // reset login status
+            AuthenticationService.ClearCredentials();
+        })();
+
+        function login() {
+            vm.dataLoading = true;
+            AuthenticationService.Login(vm.username, vm.password, function (response) {
+                if (response.success) {
+                    AuthenticationService.SetCredentials(vm.username, vm.password);
+                    $location.path('/');
+                } else {
+                    FlashService.Toaster(response.message,'error','Invalid Credentials')
+                    // FlashService.Error(response.message);
+                    vm.dataLoading = false;
+                }
+            });
+        };
+    }
+
+})();
